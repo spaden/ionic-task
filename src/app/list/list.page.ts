343af8c6b1e95dafc6e5aadadbad5065add3cd16
Dheshoju Kalyan Kumar <kalyanspade.n@gmail.com>
2020-01-24 11:55:22 +0530

    basic sidemenu and heirachy completed

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-list',
   templateUrl: 'list.page.html',
-  styleUrls: ['list.page.scss']
+  styleUrls: ['list.page.css']
 })
 export class ListPage implements OnInit {
 
@@ -12,6 +12,19 @@ export class ListPage implements OnInit {
   }
 
   ngOnInit() {
+    this.list()
+  }
+
+  list(){
+    var toggler = document.getElementsByClassName("caret")
+    var i
+
+    for (i = 0; i < toggler.length; i++) {
+      toggler[i].addEventListener("click", function() {
+        this.parentElement.querySelector(".nested").classList.toggle("active")
+        this.classList.toggle("caret-down")
+      })
+    }
   }
   // add back when alpha.4 is out
   // navigate(item) {
