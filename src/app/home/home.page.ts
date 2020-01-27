adefcce66dbbfa414158ee916b34cb41794482f8
Dheshoju Kalyan Kumar <kalyanspade.n@gmail.com>
2020-01-27 17:56:10 +0530

    Added list of assets
    Used virtual scroll feature like in recycler view from android sdk

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,50 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  items: any[] = [];
+  rotateImg = 0;
+  lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, seddo eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
-  constructor() {}
+  constructor() {
+    for (let i = 0; i < 1000; i++) {
+      this.items.push({
+        name: i + ' - ' + this.images[this.rotateImg],
+        imgSrc: this.getImgSrc(),
+        avatarSrc: this.getImgSrc(),
+        imgHeight: Math.floor(Math.random() * 50 + 150),
+        content: this.lorem.substring(0, Math.random() * (this.lorem.length - 100) + 100)
+      });
+
+      this.rotateImg++;
+      if (this.rotateImg === this.images.length) {
+        this.rotateImg = 0;
+      }
+    }
+    console.log(this.items)
+  
+  }
+
+images = [
+  'bandit',
+  'batmobile',
+  'blues-brothers',
+  'bueller',
+  'delorean',
+  'eleanor',
+  'general-lee',
+  'ghostbusters',
+  'knight-rider',
+  'mirth-mobile'
+];
+
+getImgSrc() {
+  const src = 'https://dummyimage.com/600x400/${Math.round( Math.random() * 99999)}/fff.png';
+  this.rotateImg++;
+  if (this.rotateImg === this.images.length) {
+    this.rotateImg = 0;
+  }
+  return src;
+}
+
 
 }
