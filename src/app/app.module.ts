4dead728d182585ed2f815f1f1099c43c84a5a43
Dheshoju Kalyan Kumar <kalyanspade.n@gmail.com>
2020-01-28 18:17:36 +0530

    added the qr code part
    Used ionic qr code scanner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { QRScanner } from '@ionic-native/qr-scanner/ngx';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
@@ -18,8 +18,10 @@ import { AppRoutingModule } from './app-routing.module';
     AppRoutingModule
   ],
   providers: [
+    QRScanner,
     StatusBar,
     SplashScreen,
+    
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
---
+++ src/app/scan/scan.page.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
+import { Platform } from '@ionic/angular';
+
+@Component({
+  selector: 'app-scan',
+  templateUrl: './scan.page.html',
+  styleUrls: ['./scan.page.scss'],
+})
+export class ScanPage implements OnInit {
+  scanSub: any;
+  qrText: string;
+
+ 
+  constructor(public platform: Platform,public qrScanner: QRScanner) {
+    this.platform.backButton.subscribeWithPriority(0, () => {
+      document.getElementsByTagName('body')[0].style.opacity = '1';
+      this.scanSub.unsubscribe();
+    });
+   }
+
+  ngOnInit() {
+    
+  }
+  
+  startScanning() {
+    // Optionally request the permission early
+    this.qrScanner.prepare().
+      then((status: QRScannerStatus) => {
+        if (status.authorized) {
+          this.qrScanner.show();
+          this.scanSub = document.getElementsByTagName('body')[0].style.opacity = '0';
+          debugger
+          this.scanSub = this.qrScanner.scan()
+            .subscribe((textFound: string) => {
+              document.getElementsByTagName('body')[0].style.opacity = '1';
+              this.qrScanner.hide();
+              this.scanSub.unsubscribe();
+
+              this.qrText = textFound;
+            }, (err) => {
+              alert(JSON.stringify(err));
+            });
+
+        } else if (status.denied) {
+        } else {
+
+        }
+      })
+      .catch((e: any) => console.log('Error is', e));
+  }
+  
+}
