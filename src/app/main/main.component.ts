b1b415af32ca473b6b0e2056b52fbd1f38b641df
Dheshoju Kalyan Kumar <kalyanspade.n@gmail.com>
2020-02-04 15:54:03 +0530

    updated
    Need to implement the back button and improving the item list design

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';  
 
 @Component({
   selector: 'app-main',
@@ -13,28 +14,71 @@ export class MainComponent implements OnInit {
       icon: 'home'
     },
     {
-      title: 'User Management',
-      url: '/list',
-      icon: 'list'
-    },
-    {
       title: 'Asset Management',
       url: '/list',
       icon: 'list'
     },
     {
-      title: 'Operation Management',
+      title: 'AMC',
       url: '/list',
       icon: 'list'
     },
     {
-      title: 'Overview',
+      title: 'PMC',
       url: '/list',
       icon: 'list'
     }
   ];
-  constructor() { }
+  constructor(public alertCtrl: AlertController) { }
 
   ngOnInit() {}
 
+  async showAlert() {  
+    const alert = await this.alertCtrl.create({  
+      header: 'Location/Building',  
+      subHeader: 'Sequence',  
+      message: 'USA -> Los Angeles -> California -> SpaceX -> Building Q3',  
+      buttons: ['OK']  
+    });  
+    await alert.present();  
+    const result = await alert.onDidDismiss();  
+    console.log(result);  
+  }
+
+  async swithRole() {  
+    const alert = await this.alertCtrl.create({
+      header: "Testing",
+      inputs :[
+        {
+          name : 'Radio 1',
+          type: 'radio',
+          label: 'Standard user',
+          value: 'value1',
+          checked: true
+        },
+        {
+          name: 'radio2',
+          type: 'radio',
+          label: 'Admin',
+          value: 'value2'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: (data) => {
+            console.log('Confirm Ok', data);
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
 }
